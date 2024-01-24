d1ab839b4723f1f1bc8403bcb199b4fcfe61a706
Acer-User <Acer-User@DESKTOP-NSD4PLV>
2024-01-24 23:36:32 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace QuizApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                textBox1.Text = "correct";
+            }
+            else 
+            {
+                textBox1.Text = "incorrect";
+            }
+
+
+
+
+                
+
+        }
+    }
+}
